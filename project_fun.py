03821e0df3ba1ae220dde4b9398264da566ec463
Phil W <pwerdenbach@posteo.de>
2022-10-10 13:45:15 +0200

    added error analysis

--- project_fun.py
+++ project_fun.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.metrics import mean_squared_error, r2_score
 
 
 def outcome(time_delay):
@@ -63,6 +65,70 @@ def get_airport_info(data, data_air, location='ARRSTN', cols=['country']):
     data.reset_index(drop=True, inplace=True)
 
     return data
+
+
+# source: ds-predictive-regression, notebook 1
+# Calculate metric
+def calculate_metrics(y_train, y_pred_train, y_test, y_pred_test):
+    """Calculate and print out RMSE and R2 for train and test data
+
+    Args:
+        y_train (array): true values of y_train
+        y_pred_train (array): predicted values of model for y_train
+        y_test (array): true values of y_test
+        y_pred_test (array): predicted values of model for y_test
+    """
+
+    print("Metrics on training data") 
+    rmse = np.sqrt(mean_squared_error(y_train,y_pred_train))
+    r2 = r2_score(y_train,y_pred_train)
+    print("RMSE:", round(rmse, 3))
+    print("R2:", round(r2, 3))
+    print("---"*10)
+    
+    # Calculate metric
+    print("Metrics on test data")  
+    rmse = mean_squared_error(y_test, y_pred_test, squared=False)
+    # you can get the same result with this line:
+    # rmse = np.sqrt(mean_squared_error(y_test,y_pred_test))
+
+    r2 = r2_score(y_test,y_pred_test)
+    print("RMSE:", round(rmse, 3))
+    print("R2:", round(r2, 3))
+    print("---"*10)    
+
+
+# source: ds-predictive-regression, notebook 1
+def error_analysis(y_test, y_pred_test):
+    """Generated true vs. predicted values and residual scatter plot for models
+
+    Args:
+        y_test (array): true values for y_test
+        y_pred_test (array): predicted values of model for y_test
+    """     
+    # Calculate residuals
+    residuals = y_test - y_pred_test
+    
+    # Plot real vs. predicted values 
+    fig, ax = plt.subplots(1,2, figsize=(15, 5))
+    plt.subplots_adjust(right=1)
+    plt.suptitle('Error Analysis')
+    
+    ax[0].scatter(y_pred_test, y_test, color="#FF5A36", alpha=0.7)
+    ax[0].plot([-400, 350], [-400, 350], color="#193251")
+    ax[0].set_title("True vs. predicted values", fontsize=16)
+    ax[0].set_xlabel("predicted values")
+    ax[0].set_ylabel("true values")
+    ax[0].set_xlim((y_pred_test.min()-10), (y_pred_test.max()+10))
+    ax[0].set_ylim((y_test.min()-40), (y_test.max()+40))
+    
+    ax[1].scatter(y_pred_test, residuals, color="#FF5A36", alpha=0.7)
+    ax[1].plot([-400, 350], [0,0], color="#193251")
+    ax[1].set_title("Residual Scatter Plot", fontsize=16)
+    ax[1].set_xlabel("predicted values")
+    ax[1].set_ylabel("residuals")
+    ax[1].set_xlim((y_pred_test.min()-10), (y_pred_test.max()+10))
+    ax[1].set_ylim((residuals.min()-10), (residuals.max()+10));
 
 
 #MAIN function
